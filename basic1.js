24267590ad828e499b3ddcfd4f4c975394da2d4a
Manu Thamer <yanceen@yahoo.com>
2020-05-28 11:20:55 +0200

    basic exercise 2

--- basic1.js
+++ basic1.js
@@ -11,6 +11,38 @@ function submitData() {
 		output.style.color = 'red';
 	}
 
-	output.innerHTML = `Your first name is ${firstName}. <br> Your last name is ${lastName}. <br> You are ${age} years old.`;
+
+	prof = document.getElementById("prof").selectedIndex;
+
+	job = document.getElementById("prof").value;
+
+	switch(prof){
+			case 0:
+			output.style.backgroundColor = "purple";
+			break;
+
+			case 1:
+			output.style.backgroundColor = "blue";
+			break;
+
+			case 2:
+			output.style.backgroundColor = "grey";
+			break;
+
+			case 3:
+			output.style.backgroundColor = "skyblue";
+			break;
+
+			default:
+			output.style.backgroundColor = "white";
+			break;		
+	}
+
+
+
+
+
+	output.innerHTML = `Your first name is ${firstName}. <br> Your last name is ${lastName}. 
+	<br> You are ${age} years old and you work in the field of ${job}.`;
 	return false;
 }
